dc00fadacc044fb27ad1e449b00f6cd0e9a373f8
bruno23cv <bruno23cv@gmail.com>
2023-11-02 23:57:59 -0300

    preparando a interface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import customtkinter
-janela = customtkinter.CTk()
 
+janela = customtkinter.CTk()
 janela.geometry("800x400")
 janela.title("Sistema de Cadastro")
 janela.resizable(width=False, height=False)
@@ -8,5 +8,6 @@ janela._set_appearance_mode("light")
 
 frame1 = customtkinter.CTkFrame(janela,width=570,height=330).place(x=10,y=30)
 frame2 = customtkinter.CTkFrame(janela,width=180,height=180).place(x=590,y=30)
+botao1 = customtkinter.CTkButton(frame1, text="Novo", corner_radius=10).place(x=615,y=40)
 
 janela.mainloop()
