b26f8cb8428835dafc7db68ac97798f90a6dfd03
bruno23cv <bruno23cv@gmail.com>
2023-11-06 16:18:14 -0300

    criando a interface

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ janela._set_appearance_mode("light")
 
 frame1 = customtkinter.CTkFrame(janela,width=570,height=330).place(x=10,y=30)
 frame2 = customtkinter.CTkFrame(janela,width=180,height=180).place(x=590,y=30)
+
+labelnome = customtkinter.CTkLabel(janela,text="Nome: ")
+labelnome.place(x=15,y=37)
+
+textbox = customtkinter.CTkTextbox(janela,width=300,height=30,border_spacing=2)
+textbox.place(x=65,y=35)
+textbox.insert("0.0","Bruno César Vilela")
+
 botao1 = customtkinter.CTkButton(frame1, text="Novo", corner_radius=10).place(x=615,y=40)
 
 janela.mainloop()
